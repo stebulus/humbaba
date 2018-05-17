30c81ab7d15ec324f4c13cb2216c463ff7247329
Steven Taschuk <steven@amotlpaa.org>
2018-05-16 20:14:06 -0400

    test: Run specified tests only, if any specified

--- test.js
+++ test.js
@@ -55,9 +55,7 @@ function assertNotSame(notExpected, actual) {
 }
 exports.assertNotSame = assertNotSame;
 
-function runTests(tests) {
-  var keys = Object.keys(tests);
-  keys.sort();
+function runTests(tests, keys) {
   var failures = 0;
   for (var i = 0; i < keys.length; i++) {
     try {
@@ -79,6 +77,13 @@ function runTests(tests) {
 exports.runTests = runTests;
 
 function main(tests) {
-  process.exit(runTests(tests));
+  var testsToRun;
+  if (process.argv.length <= 2) {
+    testsToRun = Object.keys(tests);
+    testsToRun.sort();
+  } else {
+    testsToRun = process.argv.slice(2);
+  }
+  process.exit(runTests(tests, testsToRun));
 }
 exports.main = main;
