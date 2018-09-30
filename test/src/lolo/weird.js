c57edd0e1e6563b43acc32a3b2567923e269bfc4
Steven Taschuk <steven@amotlpaa.org>
2018-09-30 17:32:48 -0400

    Test detection of weird code

---
+++ test/src/lolo/weird.js
@@ -0,0 +1,16 @@
+var codegen = require('../../../lolo-codegen');
+var t = require('tap');
+
+t.throws(function () {
+  codegen.expr({'weird': 3}, function () {});
+}, /weird/, 'throw for objects without any of the expected keys');
+
+t.throws(function () {
+  codegen.expr({'casei': 3, 'of': [[3, 3], [[1661], 3]]}, function () {});
+}, /weird/, 'throw for casei with nonliteral matcher');
+
+t.throws(function () {
+  codegen.module({'declarations': [
+    {'weird': 3}
+  ]}, function () {});
+}, /weird/, 'throw for declarations without any of the expected keys');
